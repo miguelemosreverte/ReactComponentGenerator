d6928a6cab448627fbbdf70efabd622c9268de77
Hector Bulgarini <hbulgarini@overactive.com>
2018-11-07 13:36:26 -0300

    adding help

--- test/MiguelLemosStateless/MiguelLemosStateless.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MiguelLemosStateless from './MiguelLemosStateless';
-
-it('MiguelLemosStateless initial render', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MiguelLemosStateless />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
